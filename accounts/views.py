2acfb65b87a52372b23efd83ec6bd2539d42b266
nickmaia <nicolemaiaargondizzi02@gmail.com>
2022-12-12 20:35:45 -0300

    feat: adc requirements

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.views import generic
 
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm #Classe de um formulario
-    success_url = reverse_lazy('login') #O usuario vai se cadastrar e vai ser redirecionado para o login
-    template_name = 'registration/register.html'
+    form_class = UserCreationForm  # Classe de um formulario
+    success_url = reverse_lazy(
+        "login"
+    )  # O usuario vai se cadastrar e vai ser redirecionado para o login
+    template_name = "registration/register.html"
